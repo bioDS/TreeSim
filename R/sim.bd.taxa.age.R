9c945c02a01919798fab8a2d0a1abce991be5153
Tanja Stadler <tanja.stadler@env.ethz.ch>
2011-03-20 00:00:00 +0000

    version 1.1

--- R/sim.bd.taxa.age.R
+++ R/sim.bd.taxa.age.R
@@ -1,6 +1,6 @@
 sim.bd.taxa.age <-
 function(n,numbsim,lambda,mu,frac=1,age,mrca=FALSE){
-	phy<-sim2.bd.fast.age(n,numbsim,lambda,mu,frac,age,mrca=FALSE)
+	phy<-sim2.bd.fast.age(n,numbsim,lambda,mu,frac,age,mrca)
 	phy
 }
 
